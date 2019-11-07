c7a32d6a608070bacafdbf9bb0220d48a2b41bfd
Pavel Anni <panni@redhat.com>
2019-11-07 14:03:27 -0500

    Add removal of ^M line endings

--- verify.sh
+++ verify.sh
@@ -14,9 +14,11 @@ fi
 REPORT_FILE=$1
 SIGNED_FILE=$(mktemp /tmp/grading_report_XXXX)
 # Cut the headers before the course name
-sed '/ocp_adv_infra/,$!d' $REPORT_FILE > $SIGNED_FILE
+sed '/ocp_adv_infra/,$!d' "$REPORT_FILE" > $SIGNED_FILE
 split -l $(($(wc -l < $SIGNED_FILE) - 1)) $SIGNED_FILE
 ORIG_FILE=$(awk '{print $2}' xab)
+# Remove ^M line endings
+sed -i -e "s///" xaa
 mv xaa $ORIG_FILE
 # Check if there are FAILs in the report
 if grep -q FAIL $ORIG_FILE ; then
@@ -36,5 +38,5 @@ else
     echo ""
 fi
 
-#rm $SIGNED_FILE
-#rm xab
+rm $SIGNED_FILE
+rm xab
